5d74729ea567b0d9956d216866c53e3f69721bde
LuizWT <xluiz452@gmail.com>
2025-07-08 22:07:06 -0300

    Atualiza experiência

--- app/constants/experiences.tsx
+++ app/constants/experiences.tsx
@@ -5,14 +5,14 @@ export const experiences = [
     period: "2025-03 - O momento",
     role: "(Estágio) Suporte Técnico",
     description:
-      "Na área de Suporte Técnico, presto assistência em sistemas VoIP, diagnosticando falhas e prestando o suporte necessário para usuários do sistema."
+      "• Atuo com suporte técnico prestando atendimento via telefone e sistema CRM WAY, realizando a tratativa e resolução de chamados de forma ágil e organizada, priorizando a resolução no primeiro contato. Também sou responsável pelo follow-up com os clientes, garantindo a satisfação e o encerramento adequado dos chamados. Auxilio no controle patrimonial da empresa, mantendo o inventário de equipamentos atualizado. Além disso, participei de treinamentos técnicos voltados às soluções SUITE, Softswitch e CRM WAY, o que me capacitou a aplicar essas tecnologias com domínio em configurações, operação e funcionalidades internas."
   },
   {
     company: "Optera Sistemas",
     period: "2024-04 - 2024-08",
     role: "(Estágio) Desenvolvedor WEB",
     description:
-      "Na área de Análise e Desenvolvimento de Sistemas, atuei na criação de soluções eficazes para desafios técnicos diversos, colaborando com a equipe de QA para identificar e resolver bugs, garantindo entregas de alta qualidade. Implementei práticas de código limpo, o que reduziu significativamente o tempo de manutenção do software, além de utilizar o método Kanban para otimizar o fluxo de trabalho e trazer maior eficiência ao gerenciamento de tarefas. Desenvolvi e mantive mais de três sites responsivos com Ruby on Rails, aprimorando a experiência do usuário. Também revisei e refatorei códigos para evitar erros que comprometessem o funcionamento dos programas e ofereci suporte técnico aos clientes, contribuindo para a resolução de problemas e a manutenção da satisfação dos usuários.",
+      "• Na área de Análise e Desenvolvimento de Sistemas, atuei na criação de soluções eficazes para desafios técnicos diversos, colaborando com a equipe de QA para identificar e resolver bugs, garantindo entregas de alta qualidade. Implementei práticas de código limpo, o que reduziu significativamente o tempo de manutenção do software, além de utilizar o método Kanban para otimizar o fluxo de trabalho e trazer maior eficiência ao gerenciamento de tarefas. Desenvolvi e mantive mais de três sites responsivos com Ruby on Rails, aprimorando a experiência do usuário. Também revisei e refatorei códigos para evitar erros que comprometessem o funcionamento dos programas e ofereci suporte técnico aos clientes, contribuindo para a resolução de problemas e a manutenção da satisfação dos usuários.",
   },
   {
     company: "Serviço Social da Indústria (SESI 021)",
